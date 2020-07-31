13433c2a5ba6b342fde6deff0f859777687f5a3d
Sam Sverko <sam.sverko@gmail.com>
2020-07-31 14:57:59 -0400

    Add material-ui/core dependency. - Update app with starter code.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-function App() {
-  return <h1>Welcome</h1>
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
 }
 
 export default App
